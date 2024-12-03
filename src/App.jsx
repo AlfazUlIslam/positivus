b02b60511638dc7e72d214d2a410499b217415c3
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-12-03 17:13:51 +0600

    Project setup and homepage assets added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='app'>
+      <div className="text-7xl text-center">Hi!</div>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        positivusGreen: "#B9FF66",
+        positivusGrey: "#F3F3F3",
+        positivusDark: "#191A23"
+      }
+    },
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px"
+    },
+    container: {
+      center: true
+    },
+    fontFamily: {
+      spaceGrotesk: "'Space Grotesk', sans-serif"
+    }
+  },
+  plugins: [],
+}
+
