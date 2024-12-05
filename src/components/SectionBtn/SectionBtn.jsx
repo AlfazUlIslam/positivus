975db1ebe065040789d6e19706b65d709e993b27
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-12-05 15:45:09 +0600

    Section button component added

---
+++ src/components/SectionBtn/SectionBtn.jsx
@@ -0,0 +1,19 @@
+const SectionBtn = (props) => {
+    const { styles, content } = props
+        
+    return (
+        // Request quote button
+        // bg-transparent hover:bg-positivusDark
+        <button className={`${styles} h-[50px] 
+        rounded-[14px] border 
+        border-positivusDark font-spaceGrotesk 
+        font-normal text-[14px] text-black 
+        cursor-pointer transition-all duration-1000 
+        hover:text-white 
+        active:scale-90 sm:text-[16px] lg:text-[14px] 
+        xl:h-[70px] xl:text-[20px]`}>
+            {content}
+        </button>
+    )
+}
+export default SectionBtn
