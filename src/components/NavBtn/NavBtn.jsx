34498595c35c10c4044fdcb8266552dd4cf9aeda
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-12-05 17:17:12 +0600

    Nav button component added

---
+++ src/components/NavBtn/NavBtn.jsx
@@ -0,0 +1,15 @@
+const NavBtn = () => {
+  return (
+    <button className="w-[150px] 
+    h-[50px] bg-transparent rounded-[14px] border 
+    border-positivusDark font-spaceGrotesk 
+    font-normal text-[14px] text-black 
+    cursor-pointer transition-all duration-1000 
+    hover:bg-positivusDark hover:text-white 
+    active:scale-90 sm:w-[180px] sm:text-[16px] 
+    lg:text-[14px] xl:w-[230px] xl:h-[70px] xl:text-[20px]">
+        Request a quote
+    </button>
+  )
+}
+export default NavBtn
