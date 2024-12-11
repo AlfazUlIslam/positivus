d2d3b0e9191597947532720c54d6b83efa9b61f2
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-12-11 15:07:39 +0600

    Services modal popup tablet styles added

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -24,11 +24,10 @@ const Modal = (props) => {
         // Modal
         <div className={`modal ${toggleModal ? "block" : "hidden"}`}>
             {/* Modal content */}
-            {/* w-[750px] h-[500px] rounded-[45px] 
-                p-[50px] */}
             <div 
-                className="w-[300px] h-auto rounded-[23px] 
-                p-[20px] flex justify-between modal_content"
+                className="modal_content w-[300px] h-auto rounded-[23px] 
+                p-[20px] flex justify-between sm:w-[500px] xl:w-[750px] 
+                xl:h-[500px] xl:rounded-[45px] xl:p-[50px]"
                 style={modalContent}
             >
                 <ColumnOne styles={`w-1/2 flex flex-col items-start 
@@ -38,21 +37,22 @@ const Modal = (props) => {
                         <Title 
                             styles={`${theme === "grey" ? 
                             "bg-positivusGreen text-black" : 
-                            "bg-white text-black"}`}
+                            "bg-white text-black"} text-[16px] 
+                            xl:text-[18px]`}
                             content={titleOne}
                         />
                         <Title 
                             styles={`${theme === "grey" ? 
                             "bg-positivusGreen text-black" : 
-                            "bg-white text-black"}`}
+                            "bg-white text-black"} text-[16px] 
+                            xl:text-[18px]`}
                             content={titleTwo}
                         />
                     </div>
                     {/* Service description */}
-                    {/* text-md */}
                     <p className={`font-spaceGrotesk font-semibold text-[10px] 
                     leading-[28px] ${theme === "black" ? "text-white" : 
-                    "text-black"}`}>
+                    "text-black"} sm:text-[12px] xl:text-[14px]`}>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam expedita pariatur esse exercitationem tempore officiis saepe ullam, vel maiores ab numquam voluptatibus. Quidem quasi, placeat, esse consequuntur iure saepe pariatur tenetur id molestiae cupiditate, odit fuga eius? Voluptates accusantium, eos quidem, non quibusdam aperiam nulla minima tempora fugiat est similique!
                     </p>
                 </ColumnOne>
@@ -60,9 +60,8 @@ const Modal = (props) => {
                     {/* Cancel button container */}
                     <div className="">
                         {/* Cancel button */}
-                        {/* text-5xl */}
                         <button 
-                            className="text-3xl"
+                            className="text-3xl sm:text-5xl"
                             style={cancelButton}
                             onClick={handleToggleModal}
                         >
@@ -71,9 +70,8 @@ const Modal = (props) => {
                     </div>
                     {/* Service logo */}
                     <div>
-                        {/* w-auto */}
                         <img 
-                            className="w-[100px]"
+                            className="w-[100px] sm:w-[150px] xl:w-auto"
                             src={logo} 
                             alt={altText} 
                         />
