aa7ef381ae99b96d482ceeca8e8d51e76c58f718
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-12-12 16:31:57 +0600

    Studies data added

--- src/data/index.js
+++ src/data/index.js
@@ -51,3 +51,18 @@ export const servicesCardsData = [
         altText: "Analytics and Tracking"
     },
 ]
+
+export const studiesData = [
+    {
+        id: uuidv4(),
+        description: "For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales."
+    },
+    {
+        id: uuidv4(),
+        description: "For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords and a 200% increase in organic traffic."
+    },
+    {
+        id: uuidv4(),
+        description: "For a national retail chain, we created a social media marketing campaign that increased followers by 25% and generated a 20% increase in online sales."
+    },
+]
