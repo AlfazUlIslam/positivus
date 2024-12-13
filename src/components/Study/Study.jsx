1af37b6c487dda441db12586e35995479027da5d
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-12-13 10:51:02 +0600

    Case studies mobile styles added

--- src/components/Study/Study.jsx
+++ src/components/Study/Study.jsx
@@ -5,12 +5,15 @@ const Study = (props) => {
         
     return (
         // Study
-        <div className={`${styles} px-[64px] flex flex-col 
-        justify-start items-start gap-[20px] 
-        ${elemIndex !== 0 ? "border-l-[1px] border-white" : ""}`}>
+        // ${elemIndex !== 0 ? "border-l-[1px] border-white" : ""} --
+        // w-auto px-[64px] py-0
+        <div className={`${styles} w-[260px] 
+        px-[20px] py-[20px] flex flex-col 
+        justify-start items-start gap-[20px]`}>
             {/* Study description */}
-            <p className="w-[286px] font-spaceGrotesk 
-            font-normal text-[18px] text-white">
+            {/* text-[18px] */}
+            <p className="w-[220px] font-spaceGrotesk 
+            font-normal text-[12px] text-white">
                 {description}
             </p>
             {/* Learn more button */}
@@ -19,8 +22,9 @@ const Study = (props) => {
             items-center gap-[15px] transition-all 
             duration-1000 hover:border-positivusGreen">
                 {/* Learn more button text */}
+                {/* text-[20px] */}
                 <span className="font-spaceGrotesk 
-                font-normal text-[20px] text-positivusGreen">
+                font-normal text-[14px] text-positivusGreen">
                     Learn more
                 </span>
                 {/* Striped arrow icon */}
--- src/sections/CaseStudies/CaseStudies.jsx
+++ src/sections/CaseStudies/CaseStudies.jsx
@@ -6,19 +6,24 @@ const CaseStudies = () => {
   return (
     // Case studies
     <section id="case-studies">
-        <Container styles={`px-6 py-[70px]`}>
-            <Content styles={`flex justify-start items-center 
-            gap-[40px]`}>
+        {/* py-[70px] */}
+        <Container styles={`px-6 py-[40px]`}>
+            {/* flex-col justify-start gap-[40px] */}
+            <Content styles={`flex flex-col justify-center 
+            items-center gap-[20px]`}>
                 <Heading content={"Case Studies"} />
+                {/* w-[580px] text-start */}
                 <Description 
-                    styles={`w-[580px]`}
+                    styles={`w-[280px] text-center`}
                     content={"Explore Real-Life Examples of Our Proven Digital Marketing Success through Our Case Studies"}
                 />
             </Content>
             {/* Studies */}
-            <div className="w-[100%] rounded-[45px] 
-            py-[90px] mt-[80px] bg-positivusDark flex 
-            justify-center">
+            {/* w-[100%] rounded-[5px] py-[90px] mt-[40px] 
+                mx-0 flex-row gap-0 divide-y */}
+            <div className="w-[280px] rounded-[45px] 
+            py-[40px] mt-[40px] mx-auto bg-positivusDark flex 
+            flex-col justify-center items-center divide-y">
                 {studiesData.map((studyData, index) => (
                     <Study 
                         key={studyData.id}
