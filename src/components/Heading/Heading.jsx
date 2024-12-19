808c58034bb262131efe542d697263c0aa2b7c07
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-12-19 15:44:08 +0600

    Heading component modified

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,11 +1,11 @@
 const Heading = (props) => {
-    const { content } = props
+    const { styles, content } = props
         
     return (
         // Heading
-        <h3 className="rounded-[7px] p-[10px] 
+        <h3 className={`${styles} rounded-[7px] p-[10px] 
         bg-positivusGreen font-spaceGrotesk font-medium 
-        text-[30px] text-black xl:text-[40px]">
+        text-[30px] text-black xl:text-[40px]`}>
             {content}
         </h3>
     )
