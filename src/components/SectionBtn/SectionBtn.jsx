bc77fd2731c23ed8f891d4ce14306f8ca40da603
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-12-24 19:50:57 +0600

    SectionBtn component modified

--- src/components/SectionBtn/SectionBtn.jsx
+++ src/components/SectionBtn/SectionBtn.jsx
@@ -1,15 +1,16 @@
 const SectionBtn = (props) => {
-    const { styles, content } = props
+    const { styles, type, content } = props
         
     return (
         // Section button
-        <button className={`${styles} h-[50px] 
-        rounded-[14px] bg-positivusDark font-spaceGrotesk 
-        font-normal text-[14px] text-white cursor-pointer 
-        transition-all duration-1000 hover:bg-positivusGreen 
-        active:scale-90 sm:text-[20px] sm:h-[70px] 
-        md:h-[50px] md:text-[14px] lg:h-[55px] lg:text-[18px] 
-        xl:h-[70px] xl:text-[20px]`}>
+        <button 
+            className={`${styles} h-[50px] rounded-[14px] bg-positivusDark 
+            font-spaceGrotesk font-normal text-[14px] text-white cursor-pointer 
+            transition-all duration-1000 hover:bg-positivusGreen active:scale-90 
+            sm:text-[20px] sm:h-[70px] md:h-[50px] md:text-[14px] lg:h-[55px] 
+            lg:text-[18px] xl:h-[70px] xl:text-[20px]`}
+            type={type ? type : ""}
+        >
             {content}
         </button>
     )
